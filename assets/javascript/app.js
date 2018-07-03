9bb6119806c950224cdb5e585f885a689a7efc4a
imalam64 <imalam64@gmail.com>
2018-07-03 11:27:36 -0700

    page setup, variable setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    var question = [
+        "What is the capitol of Arkansas?",
+        "What is the capitol of Texas?",
+        "What is the capitol of Wyoming?",
+        "What is the capitol of Florida?",
+        "What is the capitol of Rhode Island?"
+    ];
+    console.log( question);
+    var answer0 = ["El Dorado", "Conway", "Little Rock", "Jonesboro"]; //Little Rock
+    var answer1 = ["Austin", "Dallas", "Houston", "San Antonio"]; //Austin
+    var answer2 = ["Casper", "Cheyenne", "Jackson", "Rock Springs"]; //Cheyenne
+    var answer3 = ["Miami", "Tallahassee", "Orlando", "Tampa"]; //Tallahassee
+    var answer4 = ["Warwick", "Providence", "Cranston", "Little Compton"]; //Providence
+
+    //on click, start the timer
+
+    //display the questions
+
+    //
+});
